6275cb80c1aa041616000ece988524e8a1c05da9
Jonas Stafset <jonaswstafset@gmail.com>
2024-08-11 12:34:06 +0200

    heipådeg

--- schemaTypes/services.ts
+++ schemaTypes/services.ts
@@ -1,187 +1,252 @@
-import { IServices } from './types';
+import {IServices} from './types'
 
 const services: IServices = {
-    name: 'services',
-    title: 'Services',
-    type: 'document',
-    fieldsets: [
-        { name: 'mainSection', title: 'Main Section', options: { collapsible: true, collapsed: true } },
-        { name: 'topTextSection', title: 'Top Text Section', options: { collapsible: true, collapsed: true } },
-        { name: 'recruitingSection', title: 'Recruiting Section', options: { collapsible: true, collapsed: true } },
-        { name: 'hireSection', title: 'Hire Section', options: { collapsible: true, collapsed: true } },
-        { name: 'headhuntingSection', title: 'Headhunting Section', options: { collapsible: true, collapsed: true } },
-        { name: 'counselingSection', title: 'Counseling Section', options: { collapsible: true, collapsed: true } },
-        { name: 'middleTextSection', title: 'Middle Text Section', options: { collapsible: true, collapsed: true } },
-        { name: 'adaptedToNeedsSection', title: 'Adapted to Needs Section', options: { collapsible: true, collapsed: true } },
-        { name: 'personalityTestAndAbilityTestSection', title: 'Personality Test and Ability Test Section', options: { collapsible: true, collapsed: true } },
-    ],
-    initialValue: {
-        title: 'Services',  
+  name: 'services',
+  title: 'Services',
+  type: 'document',
+  fieldsets: [
+    {name: 'mainSection', title: 'Main Section', options: {collapsible: true, collapsed: true}},
+    {
+      name: 'topTextSection',
+      title: 'Top Text Section',
+      options: {collapsible: true, collapsed: true},
     },
-    fields: [
-        {
-            name: 'documentTitle',  
-            type: 'string',
-            title: 'Document Title'
-        },
+    {
+      name: 'recruitingSection',
+      title: 'Recruiting Section',
+      options: {collapsible: true, collapsed: true},
+    },
+    {name: 'hireSection', title: 'Hire Section', options: {collapsible: true, collapsed: true}},
+    {
+      name: 'headhuntingSection',
+      title: 'Headhunting Section',
+      options: {collapsible: true, collapsed: true},
+    },
+    {
+      name: 'counselingSection',
+      title: 'Counseling Section',
+      options: {collapsible: true, collapsed: true},
+    },
+    {
+      name: 'middleTextSection',
+      title: 'Middle Text Section',
+      options: {collapsible: true, collapsed: true},
+    },
+    {
+      name: 'adaptedToNeedsSection',
+      title: 'Adapted to Needs Section',
+      options: {collapsible: true, collapsed: true},
+    },
+    {
+      name: 'personalityTestAndAbilityTestSection',
+      title: 'Personality Test and Ability Test Section',
+      options: {collapsible: true, collapsed: true},
+    },
+  ],
+  initialValue: {
+    title: 'Services',
+  },
+  fields: [
+    {
+      name: 'documentTitle',
+      type: 'string',
+      title: 'Document Title',
+    },
 
-        // Main Section
-        {
-            name: 'mainTitle',
-            type: 'string',
-            title: 'Title',
-            fieldset: 'mainSection'
-        },
-        {
-            name: 'mainSubTitleItems',
-            type: 'array',
-            title: 'Subtitle Items',
-            of: [{ type: 'string' }],
-            fieldset: 'mainSection'
-        },
-        
-        //Top Text Section
-        {
-            name: 'topTextTitle',
-            type: 'string',
-            title: 'Title',
-            fieldset: 'topTextSection'
-        },
+    // Main Section
+    {
+      name: 'mainTitle',
+      type: 'string',
+      title: 'Title',
+      fieldset: 'mainSection',
+    },
+    {
+      name: 'mainSubTitleItems',
+      type: 'array',
+      title: 'Subtitle Items',
+      of: [{type: 'string'}],
+      fieldset: 'mainSection',
+    },
 
-        //Recruiting Section
-        {
-            name: 'recruitingTitle',
-            type: 'string',
-            title: 'Title',
-            fieldset: 'recruitingSection'
-        },
-        {
-            name: 'recruitingTitleItems',
-            type: 'array',
-            title: 'recruiting Items',
-            of: [{ type: 'string' }],
-            fieldset: 'recruitingSection'
-        },
-        {
-            name: 'recruitingReadMoreText',
-            type: 'text',
-            title: 'Read More Text',
-            fieldset: 'recruitingSection'
-        },
+    //Top Text Section
+    {
+      name: 'topTextTitle',
+      type: 'string',
+      title: 'Title',
+      fieldset: 'topTextSection',
+    },
 
-        //Hire Section
-        {
-            name: 'hireTitle',
-            type: 'string',
-            title: 'Title',
-            fieldset: 'hireSection'
-        },
-        {
-            name: 'hireTitleItems',
-            type: 'array',
-            title: 'hire Items',
-            of: [{ type: 'string' }],
-            fieldset: 'hireSection'
-        },
-        {
-            name: 'hireReadMoreText',
-            type: 'text',
-            title: 'Read More Text',
-            fieldset: 'hireSection'
-        },
+    //Recruiting Section
+    {
+      name: 'recruitingCategory',
+      type: 'string',
+      title: 'Category',
+      fieldset: 'recruitingSection',
+    },
+    {
+      name: 'recruitingTitle',
+      type: 'string',
+      title: 'Title',
+      fieldset: 'recruitingSection',
+    },
+    {
+      name: 'recruitingTitleItems',
+      type: 'array',
+      title: 'recruiting Items',
+      of: [{type: 'string'}],
+      fieldset: 'recruitingSection',
+    },
+    {
+      name: 'recruitingReadMoreText',
+      type: 'text',
+      title: 'Read More Text',
+      fieldset: 'recruitingSection',
+    },
 
-        //Headhunting Section
-        {
-            name: 'headhuntingTitle',
-            type: 'string',
-            title: 'Title',
-            fieldset: 'headhuntingSection'
-        },
-        {
-            name: 'headhuntingTitleItems',
-            type: 'array',
-            title: 'headhunting Items',
-            of: [{ type: 'string' }],
-            fieldset: 'headhuntingSection'
-        },
-        {
-            name: 'headhuntingReadMoreText',
-            type: 'text',
-            title: 'Read More Text',
-            fieldset: 'headhuntingSection'
-        },
+    //Hire Section
+    {
+      name: 'hireCategory',
+      type: 'string',
+      title: 'Category',
+      fieldset: 'hireSection',
+    },
+    {
+      name: 'hireTitle',
+      type: 'string',
+      title: 'Title',
+      fieldset: 'hireSection',
+    },
+    {
+      name: 'hireTitleItems',
+      type: 'array',
+      title: 'hire Items',
+      of: [{type: 'string'}],
+      fieldset: 'hireSection',
+    },
+    {
+      name: 'hireReadMoreText',
+      type: 'text',
+      title: 'Read More Text',
+      fieldset: 'hireSection',
+    },
 
-        //Counseling Section
-        {
-            name: 'counselingTitle',
-            type: 'string',
-            title: 'Title',
-            fieldset: 'counselingSection'
-        },
-        {
-            name: 'counselingTitleItems',
-            type: 'array',
-            title: 'counseling Items',
-            of: [{ type: 'string' }],
-            fieldset: 'counselingSection'
-        },
-        {
-            name: 'counselingReadMoreText',
-            type: 'text',
-            title: 'Read More Text',
-            fieldset: 'counselingSection'
-        },
+    //Headhunting Section
+    {
+      name: 'headhuntingCategory',
+      type: 'string',
+      title: 'Category',
+      fieldset: 'headhuntingSection',
+    },
+    {
+      name: 'headhuntingTitle',
+      type: 'string',
+      title: 'Title',
+      fieldset: 'headhuntingSection',
+    },
+    {
+      name: 'headhuntingTitleItems',
+      type: 'array',
+      title: 'headhunting Items',
+      of: [{type: 'string'}],
+      fieldset: 'headhuntingSection',
+    },
+    {
+      name: 'headhuntingReadMoreText',
+      type: 'text',
+      title: 'Read More Text',
+      fieldset: 'headhuntingSection',
+    },
 
-        //Middle Text Section
-        {
-            name: 'middleTextTitle',
-            type: 'string',
-            title: 'Title',
-            fieldset: 'middleTextSection'
-        },
+    //Counseling Section
+    {
+      name: 'conselingCategory',
+      type: 'string',
+      title: 'Category',
+      fieldset: 'conselingSection',
+    },
 
-        //Adapted to Needs Section
-        {
-            name: 'adaptedToNeedsTitle',
-            type: 'string',
-            title: 'Title',
-            fieldset: 'adaptedToNeedsSection'
-        },
-        {
-            name: 'adaptedToNeedsTitleItems',
-            type: 'array',
-            title: 'Adapted to Needs Items',
-            of: [{ type: 'string' }],
-            fieldset: 'adaptedToNeedsSection'
-        },
-        {
-            name: 'adaptedToNeedsReadMoreText',
-            type: 'text',
-            title: 'Read More Text',
-            fieldset: 'adaptedToNeedsSection'
-        },
+    {
+      name: 'counselingTitle',
+      type: 'string',
+      title: 'Title',
+      fieldset: 'counselingSection',
+    },
+    {
+      name: 'counselingTitleItems',
+      type: 'array',
+      title: 'counseling Items',
+      of: [{type: 'string'}],
+      fieldset: 'counselingSection',
+    },
+    {
+      name: 'counselingReadMoreText',
+      type: 'text',
+      title: 'Read More Text',
+      fieldset: 'counselingSection',
+    },
 
-        //Personality Test and Ability Test Section
-        {
-            name: 'personalityTestAndAbilityTestTitle',
-            type: 'string',
-            title: 'Title',
-            fieldset: 'personalityTestAndAbilityTestSection'
-        },
-        {
-            name: 'personalityTestAndAbilityTestTitleItems',
-            type: 'array',
-            title: 'Personality Test and Ability Test Items',
-            of: [{ type: 'string' }],
-            fieldset: 'personalityTestAndAbilityTestSection'
-        },
-        {
-            name: 'personalityTestAndAbilityTestReadMoreText',
-            type: 'text',
-            title: 'Read More Text',
-            fieldset: 'personalityTestAndAbilityTestSection'
-        },
-    ],
-};
+    //Middle Text Section
+    {
+      name: 'middleTextTitle',
+      type: 'string',
+      title: 'Title',
+      fieldset: 'middleTextSection',
+    },
 
-export default services;
+    //Adapted to Needs Section
+    {
+      name: 'adaptedToNeedsCategory',
+      type: 'string',
+      title: 'Category',
+      fieldset: 'adaptedToNeedsSection',
+    },
+    {
+      name: 'adaptedToNeedsTitle',
+      type: 'string',
+      title: 'Title',
+      fieldset: 'adaptedToNeedsSection',
+    },
+    {
+      name: 'adaptedToNeedsTitleItems',
+      type: 'array',
+      title: 'Adapted to Needs Items',
+      of: [{type: 'string'}],
+      fieldset: 'adaptedToNeedsSection',
+    },
+    {
+      name: 'adaptedToNeedsReadMoreText',
+      type: 'text',
+      title: 'Read More Text',
+      fieldset: 'adaptedToNeedsSection',
+    },
+
+    //Personality Test and Ability Test Section
+    {
+      name: 'personalityTestAndAbilityTestCategory',
+      type: 'string',
+      title: 'Category',
+      fieldset: 'personalityTestAndAbilityTestSection',
+    },
+    {
+      name: 'personalityTestAndAbilityTestTitle',
+      type: 'string',
+      title: 'Title',
+      fieldset: 'personalityTestAndAbilityTestSection',
+    },
+    {
+      name: 'personalityTestAndAbilityTestTitleItems',
+      type: 'array',
+      title: 'Personality Test and Ability Test Items',
+      of: [{type: 'string'}],
+      fieldset: 'personalityTestAndAbilityTestSection',
+    },
+    {
+      name: 'personalityTestAndAbilityTestReadMoreText',
+      type: 'text',
+      title: 'Read More Text',
+      fieldset: 'personalityTestAndAbilityTestSection',
+    },
+  ],
+}
+
+export default services
--- schemaTypes/types.ts
+++ schemaTypes/types.ts
@@ -1,132 +1,138 @@
 export interface IReview {
-    _type: 'review';
-    title: string;       
-    author: string;
-    authorImage: {       
-        _type: 'image';
-    };
-    text: string;        
+  _type: 'review'
+  title: string
+  author: string
+  authorImage: {
+    _type: 'image'
+  }
+  text: string
 }
 
 export interface IHomepage {
-    _type: 'homepage';
-    mainTitle: string;
-    subTitle: string;
-    heroImages: Array<{
-        _type: 'image';
-        asset: {
-            _ref: string;
-        };
-    }>;
-    reviewsTitle: string;
-    reviews: IReview[];
-    subjectAreasTitle: string;
-    subjectAreasContainer1Title: string;
-    container1Items: string[];
-    subjectAreasContainer2Title: string;
-    container2Items: string[];
-    subjectAreasFooterText: string;
-    jobbkretserSectionTitle: string;
-    jobbkretserContainerItems: string[];
-    jobbkretserImage: {
-        _type: 'image';
-        asset: {
-            _ref: string;
-        };
-    };
-    jobbkretserSectionReadMore: string;
-    partnerCertificationSectionTitle: string;
-    partnerCertificationSectionImages: Array<{
-        _type: 'image';
-        asset: {
-            _ref: string;
-        };
-    }>;
-    bottomSectionTitle: string;
+  _type: 'homepage'
+  mainTitle: string
+  subTitle: string
+  heroImages: Array<{
+    _type: 'image'
+    asset: {
+      _ref: string
+    }
+  }>
+  reviewsTitle: string
+  reviews: IReview[]
+  subjectAreasTitle: string
+  subjectAreasContainer1Title: string
+  container1Items: string[]
+  subjectAreasContainer2Title: string
+  container2Items: string[]
+  subjectAreasFooterText: string
+  jobbkretserSectionTitle: string
+  jobbkretserContainerItems: string[]
+  jobbkretserImage: {
+    _type: 'image'
+    asset: {
+      _ref: string
+    }
+  }
+  jobbkretserSectionReadMore: string
+  partnerCertificationSectionTitle: string
+  partnerCertificationSectionImages: Array<{
+    _type: 'image'
+    asset: {
+      _ref: string
+    }
+  }>
+  bottomSectionTitle: string
 }
 
 export interface IServices {
-    _type: 'services';
-    documentTitle: string;
-    mainTitle: string;
-    mainSubTitleItems: string[];
-    topTextTitle: string;
-    recruitingTitle: string;
-    recruitingTitleItems: string[];
-    recruitingReadMoreText: string;
-    hireTitle: string;
-    hireTitleItems: string[];
-    hireReadMoreText: string;
-    headhuntingTitle: string;
-    headhuntingTitleItems: string[];
-    headhuntingReadMoreText: string;
-    counselingTitle: string;
-    counselingTitleItems: string[];
-    counselingReadMoreText: string;
-    middleTextTitle: string;
-    adaptedToNeedsTitle: string;
-    adaptedToNeedsTitleItems: string[];
-    adaptedToNeedsReadMoreText: string;
-    personalityTestAndAbilityTestTitle: string;
-    personalityTestAndAbilityTestTitleItems: string[];
-    personalityTestAndAbilityTestReadMoreText: string;
+  _type: 'services'
+  adaptedToNeedsCategory: string
+  headhuntingCategory: string
+  personalityTestAndAbilityTestCategory: string
+  conselingCategory: string
+  hireCategory: string
+  recruitingCategory: string
+  documentTitle: string
+  mainTitle: string
+  mainSubTitleItems: string[]
+  topTextTitle: string
+  recruitingTitle: string
+  recruitingTitleItems: string[]
+  recruitingReadMoreText: string
+  hireTitle: string
+  hireTitleItems: string[]
+  hireReadMoreText: string
+  headhuntingTitle: string
+  headhuntingTitleItems: string[]
+  headhuntingReadMoreText: string
+  counselingTitle: string
+  counselingTitleItems: string[]
+  counselingReadMoreText: string
+  middleTextTitle: string
+  adaptedToNeedsTitle: string
+  adaptedToNeedsTitleItems: string[]
+  adaptedToNeedsReadMoreText: string
+  personalityTestAndAbilityTestTitle: string
+  personalityTestAndAbilityTestTitleItems: string[]
+  personalityTestAndAbilityTestReadMoreText: string
 }
 
 export interface IEmployee {
-    _type: 'employee';  
-    name: string;
-    jobTitle: string;
-    employeeImage: {
-        _type: 'image';
-    };
-    emailText: string;
-    phoneNumber: number; 
-    employeeText: string;
-    facebook: string;
-    linkedin: string;
-    bioShort: string;
-    keywords: string[];
-    bio: string;
+  _type: 'employee'
+  name: string
+  jobTitle: string
+  employeeImage: {
+    _type: 'image'
+  }
+  emailText: string
+  phoneNumber: number
+  employeeText: string
+  facebook: string
+  linkedin: string
+  bioShort: string
+  keywords: string[]
+  bio: string
 }
 
 export interface IAbout {
-    _type: 'about';  
-    documentTitle: string;
-    mainTitle: string;
-    mainSectionText: string;
-    mainSectionImage: {
-        _type: 'image';
-    };
-    ourVisionTitle: string;
-    ourVisionText: string;
-    visionImage: {
-        _type: 'image';
-    };
-    employeesTitle: string;
-    employees: Array<{
-        _ref: string;  
-    }>;
+  _type: 'about'
+  documentTitle: string
+  mainTitle: string
+  mainSectionText: string
+  mainSectionImage: {
+    _type: 'image'
+  }
+  ourVisionTitle: string
+  ourVisionText: string
+  visionImage: {
+    _type: 'image'
+  }
+  employeesTitle: string
+  employees: Array<{
+    _ref: string
+  }>
 }
 
 export interface IContact {
-    _type: 'contact';  
-    documentTitle: string;
-    mainTitle: string;
-    mainSubTitle: string;
-    employees: Array<{
-        _ref: string;  
-    }>;
-    contactAddress: string;
-    contactPhoneNumber: string;  
-    contactEmail: string;
+  _type: 'contact'
+  documentTitle: string
+  mainTitle: string
+  mainSubTitle: string
+  employees: Array<{
+    _ref: string
+  }>
+  contactAddress: string
+  contactPhoneNumber: string
+  contactEmail: string
 }
 
 export interface IApplicants {
-    _type: 'applicants';  
-    documentTitle: string;
-    topSectionTitle: string;
-    topSectionSubTitle: string;
-    topSectionText: string;
-    bottomSectionTitle: string;
-    bottomSectionText: string;
+  _type: 'applicants'
+  documentTitle: string
+  topSectionTitle: string
+  topSectionSubTitle: string
+  topSectionText: string
+  bottomSectionTitle: string
+  bottomSectionText: string
 }
